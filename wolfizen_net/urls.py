02b4cdd33ae76e06fdf9bd35127ecb19f7e81340
Wolfizen <wolfizen@wolfizen.net>
2017-06-07 23:29:46 -0400

    Add infinite recursion page

--- wolfizen_net/urls.py
+++ wolfizen_net/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from wolfizen_net.views import RootPageView
+from wolfizen_net.views import *
 
 urlpatterns = [
-    url(r"^", RootPageView.as_view(), name="root"),
+    url(r"^$", RootPageView.as_view(), name="root"),
+    url(r"^infinite/(?P<depth>\d+)$", InfiniteRecursionView.as_view(), name="infinite-recursion"),
     url(r"^admin/", admin.site.urls),
 ]
--- wolfizen_net/views.py
+++ wolfizen_net/views.py
@@ -3,3 +3,13 @@ from django.views.generic import TemplateView
 
 class RootPageView(TemplateView):
     template_name = "wolfizen_net/root.html"
+
+class InfiniteRecursionView(TemplateView):
+    template_name = "wolfizen_net/infinite.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(InfiniteRecursionView, self).get_context_data(**kwargs)
+        context['depth'] = int(context['depth'])
+        context['next_depth'] = context['depth'] + 1
+        context['prev_depth'] = context['depth'] - 1
+        return context
