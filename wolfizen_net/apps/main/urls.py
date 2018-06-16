9ceebc1768a52e6b40a891f5a8f6560d6c78fbbc
Wolfizen <wolfizen@wolfizen.net>
2018-06-16 16:01:05 -0400

    Add keybase verification

--- wolfizen_net/apps/main/urls.py
+++ wolfizen_net/apps/main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r"^$", RootPageView.as_view(), name="root"),
     url(r"^infinite/(?P<depth>\d+)$", InfiniteRecursionView.as_view(), name="infinite-recursion"),
     url(r"^rainbow/$", RainbowTextView.as_view(), name="rainbow-text"),
+    url(r"^keybase.txt$", TemplateView.as_view(template_name="main/keybase.txt", content_type="text/plain"), name="keybase"),
 ]
