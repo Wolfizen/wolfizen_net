188f9955d34a3ca3566192f02a62dca1bcc7980d
Wolfizen <wolfizen@wolfizen.net>
2020-01-28 01:36:49 -0500

    Add Cache-Control to nginx static files, do extra reloading in deploy.sh

--- system/deploy.sh
+++ system/deploy.sh
@@ -10,18 +10,20 @@ systemctl stop wolfizen_net
 
 
 ## Clean old files and get new version
-cd /srv
+cd /srv || { echo "cd failed"; exit 1; }
 rm -rf ./wolfizen_net
 git clone https://github.com/Wolfizen/wolfizen_net.git ./wolfizen_net
 
 
 ## Production setup
-cd wolfizen_net/
+cd wolfizen_net/ || { echo "cd failed"; exit 1; }
 python3 -m virtualenv --python=python3 ./env
-source ./env/bin/activate
+. ./env/bin/activate
 python3 -m pip install -r ./requirements.txt
 ./manage.py collectstatic
 chown -R http:http .
 
 ## Start website service
+systemctl daemon-reload
 systemctl start wolfizen_net
+systemctl reload nginx
