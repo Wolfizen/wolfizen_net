5791b6531fd87cafa6990c6838dafd12d0ebf70d
Wolfizen <wolfizen@wolfizen.net>
2020-07-09 19:04:36 -0700

    Remove keybase.io verification

--- wolfizen_net/apps/main/urls.py
+++ wolfizen_net/apps/main/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     url(r"^$", RootPageView.as_view(), name="root"),
     url(r"^infinite/(?P<depth>\d+)$", InfiniteLinksView.as_view(), name="infinite-links"),
     url(r"^rainbow/$", RainbowTextView.as_view(), name="rainbow-text"),
-    url(r"^keybase.txt$", FileView.as_view(
-        file_path=os.path.join(settings.TEMPLATE_DIR, "main/keybase.txt"), content_type="text/plain"), name="keybase"),
     url(r"^resume.pdf$", FileView.as_view(
         file_path=os.path.join(settings.TEMPLATE_DIR, "main/resume.pdf"), content_type="application/pdf"), name="resume"),
 ]
